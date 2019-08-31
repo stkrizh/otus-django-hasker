44b1cb208d04b29b8a5145fc039dfe92027a6202
stkrizh <st.krizh@gmail.com>
2019-08-31 11:38:33 +0300

    Add redirect from signup view if user is already logged in

--- users/views.py
+++ users/views.py
@@ -7,17 +7,6 @@ from django.urls import reverse_lazy
 from .forms import LogInForm, SignUpForm
 
 
-class SignUp(CreateView):
-    form_class = SignUpForm
-    model = get_user_model()
-    success_url = reverse_lazy("index")
-    template_name = "users/signup.html"
-
-    def form_valid(self, *args, **kwargs):
-        messages.success(self.request, "Thank you for registration!")
-        return super().form_valid(*args, **kwargs)
-
-
 class LogIn(FormView):
     form_class = LogInForm
     success_url = reverse_lazy("index")
@@ -38,3 +27,20 @@ class LogIn(FormView):
         kwargs = super().get_form_kwargs()
         kwargs["request"] = self.request
         return kwargs
+
+
+class SignUp(CreateView):
+    form_class = SignUpForm
+    model = get_user_model()
+    success_url = reverse_lazy("index")
+    template_name = "users/signup.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            messages.info(self.request, "You are already logged in.")
+            return redirect("index")
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, *args, **kwargs):
+        messages.success(self.request, "Thank you for registration!")
+        return super().form_valid(*args, **kwargs)
