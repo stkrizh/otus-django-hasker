aa4468e3e56791059f51e6a57616db8aacdf5a80
stkrizh <st.krizh@gmail.com>
2019-09-14 11:47:00 +0300

    Don't triger signals when loading a fixture

--- questions/signals.py
+++ questions/signals.py
@@ -4,10 +4,10 @@ from django.db.models.signals import post_delete, post_save
 from .models import Answer, AnswerVote, Question, QuestionVote
 
 
-def answer_created(sender, instance, created, *args, **kwargs):
+def answer_created(sender, instance, created, raw, *args, **kwargs):
     """ Update `number_of_answers` of `Question` model.
     """
-    if created:
+    if created and not raw:
         Question.objects.filter(pk=instance.question.pk).update(
             number_of_answers=(F("number_of_answers") + 1)
         )
@@ -21,14 +21,14 @@ def answer_deleted(sender, instance, *args, **kwargs):
     )
 
 
-def vote_created(sender, instance, created, *args, **kwargs):
+def vote_created(sender, instance, created, raw, *args, **kwargs):
     """ Update `rating` of `Answer` / `Question` model after
     a `Vote` instance creation.
     """
     post_model = type(instance.to)
     qs = post_model.objects.filter(pk=instance.to.pk)
 
-    if created:
+    if created and not raw:
         qs.update(
             rating=(F("rating") + instance.value),
             number_of_votes=(F("number_of_votes") + 1),
