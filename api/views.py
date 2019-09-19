300c0fe6548df0f725d538ae227f2e4874ed0e66
stkrizh <st.krizh@gmail.com>
2019-09-19 11:53:34 +0300

    Add sort parameter to questions api view

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404
 
+from rest_framework import status
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
 from questions.models import Answer, Question
 
@@ -9,12 +11,33 @@ from .serializers import AnswerSerializer, QuestionSerializer
 
 
 class QuestionsAPIView(ListCreateAPIView):
+    VALID_SORTS = {
+        "latest": "-posted",
+        "popular": "-rating",
+        "trending": "-number_of_votes",
+    }
+
     permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+    def get(self, request, *args, **kwargs):
+        self.sort = request.query_params.get("sort", "latest")
+
+        if self.sort not in self.VALID_SORTS:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return super().get(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        queryset = Question.objects.all()
+        queryset = queryset.select_related("author")
+        queryset = queryset.prefetch_related("tags")
+        queryset = queryset.order_by(self.VALID_SORTS[self.sort])
+
+        return queryset
 
 
 class AnswersAPIView(ListCreateAPIView):
