bc89e6d03e16b98802a8c1b7a2ce8574d492940c
stkrizh <st.krizh@gmail.com>
2019-09-28 12:48:46 +0300

    Remove empty classes from admin site

--- questions/admin.py
+++ questions/admin.py
@@ -3,26 +3,8 @@ from django.contrib import admin
 from .models import Answer, AnswerVote, Question, QuestionVote, Tag
 
 
-@admin.register(Answer)
-class AnswerAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(AnswerVote)
-class AnswerVoteAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(QuestionVote)
-class QuestionVoteAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(Answer)
+admin.site.register(AnswerVote)
+admin.site.register(Question)
+admin.site.register(QuestionVote)
+admin.site.register(Tag)
